0ef82694434e6e0040a13207e12cea6a164da1bf
hazarbudak <hazar@live.com.au>
2017-01-30 21:43:38 +1100

    Added official disclaimer to UI

--- ui.R
+++ ui.R
@@ -14,5 +14,10 @@ shinyUI(fluidPage(
    ),
    mainPanel(
       textOutput('mainResults')
-   )
+   ),
+   hr(),
+   fluidRow("This ARAM Reroll Calculator isn't endorsed by Riot Games and doesn't reflect
+            the views or opinions of Riot Games or anyone officially involved in producing
+            or managing League of Legends. League of Legends and Riot Games are trademarks
+            or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.")
 ))
