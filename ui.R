276d3e0a91be56063f1517980b1f70f72e6c10c2
hazarbudak <hazar@live.com.au>
2017-02-26 18:45:36 +1100

    Added tables showing top 5 and bottom 5 champions post-reroll.

--- ui.R
+++ ui.R
@@ -14,13 +14,20 @@ shinyUI(fluidPage(
          actionButton('goButton', "Go!")
       ),
       mainPanel(
-         textOutput('mainResults')
+         htmlOutput('mainResults'),
+         hr(),
+         fluidRow(
+            splitLayout(cellWidths = c("50%", "50%"),
+                        dataTableOutput('winnerWinrates', width = "50%", height = "50%"),
+                        dataTableOutput('loserWinrates',  width = "50%", height = "50%")
+            )
+         )
       )
    ),
    hr(),
-   fluidRow("This ARAM Reroll Calculator isn't endorsed by Riot Games and doesn't reflect
+   helpText("This ARAM Reroll Calculator isn't endorsed by Riot Games and doesn't reflect
             the views or opinions of Riot Games or anyone officially involved in producing
             or managing League of Legends. League of Legends and Riot Games are trademarks
             or registered trademarks of Riot Games, Inc. League of Legends © Riot Games, Inc.")
-   
+
 ))
